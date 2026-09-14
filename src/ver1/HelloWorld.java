b077a0423d2bad7cfc91e4d9eab5dff64a4c065c
Nathalie Arevalo <nathaliemarevalo@gmail.com>
2026-09-14 10:23:30 -0700

    Enhancement to the app

--- src/ver1/HelloWorld.java
+++ src/ver1/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello from IntelliJ");
         System.out.println("Nat is working locally on her branch");
+
     }
 }
